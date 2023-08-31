ebd91c0f7e3e460824c2ba3fe114a5093b59f555
codelearnerrr <jeferson.eiji@icloud.com>
2023-08-31 10:41:15 -0300

    feat: added dynamic scripts to css and styling content from cdn in front-page

--- front-page.php
+++ front-page.php
@@ -11,11 +11,8 @@
     <link rel="shortcut icon" href="images/logo.png"> 
     
     <!-- FontAwesome CSS-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 	<!-- Bootstrap CSS-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
 	<!-- Theme CSS -->
-	<!-- <link rel="stylesheet" href="css/style.css"> -->
     <?php 
         wp_head();
     ?>
--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 function followandrew_register_styles(){
-    wp_enqueue_style('followandrew-bootstrap',get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    $version = wp_get_theme()->get('Version');
+
+    wp_enqueue_style('followandrew-style',get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), $version, 'all');
+    wp_enqueue_style('followandrew-bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('followandrew-fontawesome',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'followandrew_register_styles');
