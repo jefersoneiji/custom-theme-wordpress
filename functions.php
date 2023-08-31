1a659ad134aa2ef66017e3160f2917ca0f585561
codelearnerrr <jeferson.eiji@icloud.com>
2023-08-31 11:45:54 -0300

    feat: loaded bootstrap scripts dynamically in footer for front-page

--- functions.php
+++ functions.php
@@ -8,4 +8,15 @@ function followandrew_register_styles(){
 }
 
 add_action('wp_enqueue_scripts', 'followandrew_register_styles');
+
+function followandrew_register_scripts(){
+    
+    wp_enqueue_script('followandrew-jquery',"https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('followandrew-popper',"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', true);
+    wp_enqueue_script('followandrew-bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" , array(), '4.4.1', true);
+    wp_enqueue_script('followandrew-mainjs',get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+    
+    
+}
+add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
 ?>
