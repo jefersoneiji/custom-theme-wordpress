fffe994007a593f2b27f4f5ebf900a0f67bd8cf7
codelearnerrr <jeferson.eiji@icloud.com>
2023-09-01 15:23:05 -0300

    feat: created dynamic menus

--- functions.php
+++ functions.php
@@ -6,6 +6,17 @@ function followandrew_theme_support(){
 }
 add_action('after_setup_theme', 'followandrew_theme_support');
 
+function followandrew_menus(){
+    $locations = array(
+        'primary' => 'Desktop Primary Left Sidebar',
+        'footer' => 'Footer Menu Items'
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'followandrew_menus');
+
 function followandrew_register_styles(){
     $version = wp_get_theme()->get('Version');
 
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="wp-content/themes/followandrew/assets/images/logo.png"> 
     
     <!-- FontAwesome CSS-->
 	<!-- Bootstrap CSS-->
@@ -32,6 +32,16 @@
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
 				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
 				
+				<?php 
+					wp_nav_menu(
+						array(
+							'menu' => 'primary',
+							'container' =>'',
+							'theme_location' => 'primary',
+							'items_wrap' => '<ul id="" class="navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>'
+						)
+					);
+				?>
 				<ul class="navbar-nav flex-column text-sm-center text-md-left">
 					<li class="nav-item active">
 					    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
