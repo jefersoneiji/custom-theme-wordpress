878cc64914d5f49da7be62ba8ff90beb9fbbfbc2
codelearnerrr <jeferson.eiji@icloud.com>
2023-09-04 17:19:45 -0300

    feat: customize site logo and website name from admin panel

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function followandrew_theme_support(){
     //Adds dynamic title tag support
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
 }
 add_action('after_setup_theme', 'followandrew_theme_support');
 
--- header.php
+++ header.php
@@ -21,7 +21,9 @@
 <body>
     
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
+	    <a class="site-title pt-lg-4 mb-0" href="index.html">
+			<?php echo get_bloginfo('name'); ?>
+		</a>
         
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
@@ -30,7 +32,14 @@
 			</button>
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >			
+			<?php
+				if(function_exists('the_custom_logo'))	{
+					$custom_logo_id = get_theme_mod('custom_logo');
+					$logo = wp_get_attachment_image_src($custom_logo_id);
+
+				}
+			?>
+			<img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >			
 				
 				<?php 
 					wp_nav_menu(
