3d9ddc9cae05a39b87973b93418dec952fe80263
codelearnerrr <jeferson.eiji@icloud.com>
2023-09-05 13:40:26 -0300

    feat: created article content-template, added thumbnail to post and created comment template

---
+++ comments.php
@@ -0,0 +1,160 @@
+<div class="comments-wrapper">
+
+
+    <div class="comments" id="comments">
+
+
+        <div class="comments-header">
+
+            <h2 class="comment-reply-title">
+                <?php
+                if (!have_comments()) {
+                    echo "Leave a comment";
+                } else {
+                    echo get_comments_number() . " Comments";
+                }
+                ?>
+            </h2><!-- .comments-title -->
+
+        </div><!-- .comments-header -->
+
+        <div class="comments-inner">
+
+            <div id="comment-1" class="comment even thread-even depth-1 parent">
+                <article id="div-comment-1" class="comment-body">
+                    <footer class="comment-meta">
+                        <div class="comment-author vcard">
+                            <a href="https://wordpress.org/" rel="external nofollow" class="url"><img alt=""
+                                    src="http://1.gravatar.com/avatar/d7a973c7dab26985da5f961be7b74480?s=120&amp;d=mm&amp;r=g"
+                                    class="avatar avatar-120 photo" height="120" width="120"><span class="fn">A
+                                    WordPress Commenter</span><span class="says sr-only">says:</span></a>
+                        </div>
+                        <!-- .comment-author -->
+
+                        <div class="comment-metadata">
+                            <a href="http://wordpress.mac/?p=1#comment-1">
+                                <time datetime="2020-03-25T20:07:51+00:00" title="March 25, 2020 at 8:07 pm">
+                                    March 25, 2020 at 8:07 pm </time>
+                            </a>
+
+                        </div><!-- .comment-metadata -->
+
+                    </footer><!-- .comment-meta -->
+
+                    <div class="comment-content entry-content">
+
+                        <p>Hi, this is a comment.<br>
+                            To get started with moderating, editing, and deleting comments, please visit
+                            the Comments screen in the dashboard.<br>
+                            Commenter avatars come from <a href="https://gravatar.com">Gravatar</a>.</p>
+
+                    </div><!-- .comment-content -->
+
+
+                    <footer class="comment-footer-meta">
+
+                        <span class="comment-reply"><a rel="nofollow" class="do-not-scroll comment-reply-link"
+                                href="http://wordpress.mac/?p=1&amp;replytocom=1#respond" data-commentid="1"
+                                data-postid="1" data-belowelement="div-comment-1" data-respondelement="respond"
+                                aria-label="Reply to A WordPress Commenter">Reply</a></span>
+                    </footer>
+
+
+                </article><!-- .comment-body -->
+
+                <div id="comment-2" class="comment byuser comment-author-andrew bypostauthor odd alt depth-2">
+                    <article id="div-comment-2" class="comment-body">
+                        <footer class="comment-meta">
+                            <div class="comment-author vcard">
+                                <img alt=""
+                                    src="http://2.gravatar.com/avatar/e297925dbb035adc0b67aa14da4dcc0a?s=120&amp;d=mm&amp;r=g"
+                                    class="avatar avatar-120 photo" height="120" width="120"><span
+                                    class="fn">andrew</span><span class="says sr-only">says:</span>
+                            </div>
+                            <!-- .comment-author -->
+
+                            <div class="comment-metadata">
+                                <a href="http://wordpress.mac/?p=1#comment-2">
+                                    <time datetime="2020-03-25T20:08:58+00:00" title="March 25, 2020 at 8:08 pm">
+                                        March 25, 2020 at 8:08 pm </time>
+                                </a>
+                            </div><!-- .comment-metadata -->
+
+                        </footer><!-- .comment-meta -->
+
+                        <div class="comment-content entry-content">
+
+                            <p>test reply</p>
+
+                        </div><!-- .comment-content -->
+
+
+                        <footer class="comment-footer-meta">
+
+                            <span class="comment-reply"><a rel="nofollow" class="do-not-scroll comment-reply-link"
+                                    href="http://wordpress.mac/?p=1&amp;replytocom=2#respond" data-commentid="2"
+                                    data-postid="1" data-belowelement="div-comment-2" data-respondelement="respond"
+                                    aria-label="Reply to andrew">Reply</a></span><span class="by-post-author">By Post
+                                Author</span>
+                        </footer>
+
+
+                    </article><!-- .comment-body -->
+
+                </div><!-- #comment-## -->
+            </div><!-- #comment-## -->
+            <div id="comment-3"
+                class="comment byuser comment-author-andrew bypostauthor even thread-odd thread-alt depth-1">
+                <article id="div-comment-3" class="comment-body">
+                    <footer class="comment-meta">
+                        <div class="comment-author vcard">
+                            <img alt=""
+                                src="http://2.gravatar.com/avatar/e297925dbb035adc0b67aa14da4dcc0a?s=120&amp;d=mm&amp;r=g"
+                                class="avatar avatar-120 photo" height="120" width="120"><span
+                                class="fn">andrew</span><span class="says sr-only">says:</span>
+                        </div>
+                        <!-- .comment-author -->
+
+                        <div class="comment-metadata">
+                            <a href="http://wordpress.mac/?p=1#comment-3">
+                                <time datetime="2020-03-25T20:09:31+00:00" title="March 25, 2020 at 8:09 pm">
+                                    March 25, 2020 at 8:09 pm </time>
+                            </a>
+                            <span aria-hidden="true">•</span>
+                        </div><!-- .comment-metadata -->
+
+                    </footer><!-- .comment-meta -->
+
+                    <div class="comment-content entry-content">
+
+                        <p>Comment</p>
+
+                    </div><!-- .comment-content -->
+
+
+                    <footer class="comment-footer-meta">
+
+                        <span class="comment-reply"><a rel="nofollow" class="do-not-scroll comment-reply-link"
+                                href="http://wordpress.mac/?p=1&amp;replytocom=3#respond" data-commentid="3"
+                                data-postid="1" data-belowelement="div-comment-3" data-respondelement="respond"
+                                aria-label="Reply to andrew">Reply</a></span><span class="by-post-author">By Post
+                            Author</span>
+                    </footer>
+
+
+                </article><!-- .comment-body -->
+
+            </div><!-- #comment-## -->
+
+        </div><!-- .comments-inner -->
+
+    </div><!-- comments -->
+
+    <hr class="" aria-hidden="true">
+    <div id="respond" class="comment-respond">
+        <h2 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow"
+                    id="cancel-comment-reply-link" href="/?p=1#respond" style="display:none;">Cancel
+                    reply</a></small></h2>
+    </div><!-- #respond -->
+
+</div>
--- front-page.php
+++ front-page.php
@@ -11,58 +11,7 @@
                     }
                 }
             ?>
-	    <!-- <div class='container'>
-			<p class="mt-3 lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-
-			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-				<div class="carousel-inner">
-				  <div class="carousel-item active">
-					<img src="images/slider2.jpg" class="d-block w-100" alt="1">
-				  </div>
-				  <div class="carousel-item">
-					<img src="images/slider1.jpg" class="d-block w-100" alt="2">
-				  </div>
-				  <div class="carousel-item">
-					<img src="images/slider3.jpg" class="d-block w-100" alt="3">
-				  </div>
-				</div>
-				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				  <span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				  <span class="sr-only">Next</span>
-				</a>
-			  </div>
-			  <section class="theme-bg-dark py-5 mt-4 text-center">
-				<h3 class='text-light d-block'>Subscribe to the Newsletter</h3>
-			    <form class="signup-form form-inline justify-content-center pt-3">
-                    <div class="form-group">
-                        <label class="sr-only" for="semail">Your email</label>
-                        <input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
-                </form>
-				</section>
-			<div class="row row-cols-1 mt-4 row-cols-md-2">
-				<div class="col mb-4">
-				  <div class="card">
-					<div class="card-body">
-					  <h5 class="card-title">Card title</h5>
-					  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-					</div>
-				  </div>
-				</div>
-				<div class="col mb-4">
-				  <div class="card">
-					<div class="card-body">
-					  <h5 class="card-title">Card title</h5>
-					  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-					</div>
-				  </div>
-				</div>
-		</div> -->
+	    
 	    </article>
     
 <?php
