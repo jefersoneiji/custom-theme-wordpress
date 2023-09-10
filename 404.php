c9dd51ed6fb96417de4419e59e7a200f593085bc
codeLearnerrr <jeferson.eiji@icloud.com>
2023-09-10 20:35:12 -0300

    feat: created 404 and search pages

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+?>
+
+<article class="content px-3 py-5 p-md-5">
+
+    <h1>Page Not Found</h1>
+
+    <?php 
+    get_search_form();
+    ?>
+</article>
+
+<?php
+get_footer();
+?>
